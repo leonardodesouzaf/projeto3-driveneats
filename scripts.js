adee55668e52c26fd4d83b18504589114fd6eeaa
Leonardo de Souza Ferreira <leonardosouzaf@icloud.com>
2022-06-12 00:17:30 -0300

    js-done

--- scripts.js
+++ scripts.js
@@ -1,6 +1,9 @@
 let prato;
 let bebida;
 let sobremesa;
+let valor1;
+let valor2;
+let valor3;
 
 function prato1(elemento){
     elemento.classList.add('selected');
@@ -9,7 +12,9 @@ function prato1(elemento){
     document.getElementById('c12').classList.add('off');
     document.getElementById('b13').classList.remove('selected');
     document.getElementById('c13').classList.add('off');
-    produto = "Risotto Montevero";
+    prato = "Risotto Montevero";
+    valor1 = 49.90;
+    button();
 }
 
 function prato2(elemento){
@@ -19,7 +24,9 @@ function prato2(elemento){
     document.getElementById('c11').classList.add('off');
     document.getElementById('b13').classList.remove('selected');
     document.getElementById('c13').classList.add('off');
-    produto = "Risotto Gripiccio";
+    prato = "Risotto Gripiccio";
+    valor1 = 44.90;
+    button();
 }
 
 function prato3(elemento){
@@ -29,7 +36,9 @@ function prato3(elemento){
     document.getElementById('c12').classList.add('off');
     document.getElementById('b11').classList.remove('selected');
     document.getElementById('c11').classList.add('off');
-    produto = "Risotto Ceuix";
+    prato = "Risotto Ceuix";
+    valor1 = 59.90;
+    button();
 }
 
 function bebida1(elemento){
@@ -40,6 +49,8 @@ function bebida1(elemento){
     document.getElementById('b23').classList.remove('selected');
     document.getElementById('c23').classList.add('off');
     bebida = "Soda Italiana";
+    valor2 = 19.90;
+    button();
 }
 
 function bebida2(elemento){
@@ -50,6 +61,8 @@ function bebida2(elemento){
     document.getElementById('b23').classList.remove('selected');
     document.getElementById('c23').classList.add('off');
     bebida = "Água Perrier";
+    valor2 = 14.90;
+    button();
 }
 
 function bebida3(elemento){
@@ -60,6 +73,8 @@ function bebida3(elemento){
     document.getElementById('b21').classList.remove('selected');
     document.getElementById('c21').classList.add('off');
     bebida = "Refrigerante Coca-Cola";
+    valor2 = 12.90;
+    button();
 }
 
 function sobremesa1(elemento){
@@ -70,6 +85,8 @@ function sobremesa1(elemento){
     document.getElementById('b33').classList.remove('selected');
     document.getElementById('c33').classList.add('off');
     sobremesa = "Salada de Frutas";
+    valor3 = 14.90;
+    button();
 }
 
 function sobremesa2(elemento){
@@ -80,6 +97,8 @@ function sobremesa2(elemento){
     document.getElementById('b33').classList.remove('selected');
     document.getElementById('c33').classList.add('off');
     sobremesa = "Petit Gateau Maison";
+    valor3 = 24.90;
+    button();
 }
 
 function sobremesa3(elemento){
@@ -90,4 +109,18 @@ function sobremesa3(elemento){
     document.getElementById('b31').classList.remove('selected');
     document.getElementById('c31').classList.add('off');
     sobremesa = "Cheesecake de Frutas Vermelhas";
+    valor3 = 22.90;
+    button();
 }
+
+function button(){
+    if (prato!=undefined && bebida!=undefined && sobremesa!=undefined){
+        document.getElementById('greenbutton').classList.remove('off');
+    }
+}
+
+
+function order() {
+    let mensagem =`Olá, gostaria de fazer o pedido: \n- Prato: ${prato}\n- Bebida: ${bebida}\n- Sobremesa: ${sobremesa}\n Total: R\$${(valor1+valor2+valor3).toFixed(2)}`;
+    window.open("http://wa.me/+5531999999999?text=" + encodeURIComponent(mensagem));
+  }
